b0131486999f3bfa17ba41564a3efa52e93f205d
Dylan Boland <dylan.boland@ucdconnect.ie>
2022-12-26 20:42:01 +0000

    Adding the logic to extract the outputs of the module from the module contents string (moduleContents). The Python script also iterates over the list of parameters, inputs, and outputs, and then forms three strings: paramStr, input_ports, output_ports. These strings are then concatenated together, and the resultant string is written to the output file - the output file has the following format <module_name>_instantiated.v (or .sv). There are also some headers added in that show where the inputs start and where the outputs start. Some final code tidy up is needed, as well as some more testing on bigger modules. So far, however, the Python script seems to be working as it should!

--- instantiate_module.py
+++ instantiate_module.py
@@ -32,6 +32,12 @@ a standard or conventional structure. An example is shown below:
 """
 noParamsFound = """No parameters were identified for the module."""
 noInputsIdentified = """No module inputs were identified."""
+noOutputsIdentified = """No module outputs were identified."""
+goodbyeMsg = """Success! The instantiated module is at {}."""
+# Some Header (Delimiter) Strings
+inputPortsHeader = """// ==== Inputs ===="""
+outputPortsHeader = """// ==== Outputs ====\n\t"""
+paramsHeader = """// ==== Parameters ===="""
 
 # Function to handle the input arguments
 def parsingArguments():
@@ -45,6 +51,7 @@ if __name__ == "__main__":
         print(noArgsMsg)
         exit()
     moduleFileName = args.filename # get the file name (or path) from the input arguments
+    moduleFileType = os.path.splitext(moduleFileName)[1] # get the file extension type (either ".v" or ".sv")
     # ==== Check if the File (path) Exists ====
     if os.path.isfile(moduleFileName) is False:
         print(noSuchFileMsg.format(moduleFileName))
@@ -62,8 +69,9 @@ if __name__ == "__main__":
 
     # ==== Define the Patterns to Look For ====
     module_name_pattern = re.compile(r'module\s+([a-zA-Z\_0-9]+)[\n\s]+#?[\n\s]*\(')
-    module_parameters_pattern = re.compile(r'parameter\s*([a-zA-Z\_0-9]+)\s*=')
-    module_inputs_pattern = re.compile(r'input\s*(?:wire)?\s*(?:\[[a-zA-Z0-9\_\-\:]+\])?\s*([a-zA-Z\_0-9]+)\s*,')
+    module_parameters_pattern = re.compile(r'parameter\s*(?:\[[a-zA-Z0-9\_\-\:]+\])?\s*([a-zA-Z\_0-9]+)\s*=')
+    module_inputs_pattern = re.compile(r'input\s*(?:wire)?\s*(?:\[[a-zA-Z0-9\_\-\:]+\])?\s*([a-zA-Z\_0-9]+)\s*,?')
+    module_outputs_pattern = re.compile(r'output\s*(?:wire|reg)?\s*(?:\[[a-zA-Z0-9\_\-\:]+\])?\s*([a-zA-Z\_0-9]+)\s*,?')
     # ==== Extract the Module Name first ====
     matches = re.findall(module_name_pattern, moduleContents)
     # ==== Check that only one Match was found for the Module Name ====
@@ -78,16 +86,80 @@ if __name__ == "__main__":
     # ==== Check if any Matches were found ====
     if (len(matches) == 0): # if there were zero matches
         print(noParamsFound) # print out message saying the module has no parameters (or none were identified)
+        moduleHasParams = False
     else:
         module_parameters = matches # capture the list of module parameters
         print(module_parameters)
+        moduleHasParams = True
     
     # ==== Extract the Module Inputs ====
     matches = re.findall(module_inputs_pattern, moduleContents)
     if (len(matches) == 0): # if there were no matches
         print(noInputsIdentified) # print a message saying that no module inputs were found
     else:
-        module_inputs = matches
+        module_inputs = matches # capture the list of module inputs
         print(module_inputs)
+    
+    # ==== Extract the Module Outputs ====
+    matches = re.findall(module_outputs_pattern, moduleContents)
+    if (len(matches) == 0): # if there were no matches
+        print(noOutputsIdentified) # print a message saying that no module outputs were found
+    else:
+        module_outputs = matches # capture the list of module outputs
+        print(module_outputs)
+    
+    # ==== Instantiate the Module ====
+    #
+    # ==== Step 1: Create the Parameter Part of the String (if there are any parameters) ====
+    if (moduleHasParams):
+        paramStr = ""
+        numParams = len(module_parameters) # get the number of parameters
+        for i, param in enumerate(module_parameters):
+            if (i == numParams - 1):
+                paramStr += f".{param}({param})"
+            else:
+                paramStr += f".{param}({param}),\n"
+    
+    # ==== Step 2: Create Part of the String with the Inputs ====
+    input_ports = ""
+    for input_port in module_inputs:
+        input_ports += f".{input_port}({input_port}),\n\t"
 
+    # ==== Step 3: Create Part of the String with the Outputs ====
+    output_ports = ""
+    numOutputs = len(module_outputs) # number of outputs for the module
+    for i, output_port in enumerate(module_outputs):
+        if (i != numOutputs - 1):
+            output_ports += f".{output_port}({output_port}),\n\t"
+        else:
+            output_ports += f".{output_port}({output_port})"
+    
+    # ==== Step 4: Form the Final String =====
+    instantiatedModule = "" # empty at the moment
+    if (moduleHasParams):
+        # ==== Using f (formatted) strings ====
+        instantiatedModule += f"""{module_name} #
+        (
+        {paramsHeader}
+        {paramStr}
+        ) dut
+        (
+        {inputPortsHeader}
+        {input_ports}{outputPortsHeader}{output_ports}
+        );"""
+        print(instantiatedModule)
+    else:
+        instantiatedModule += f"""{module_name}
+        (
+        {inputPortsHeader}
+        {input_ports}{outputPortsHeader}{output_ports}
+        );"""
+        print(instantiatedModule)
+    
+    # ==== Step 5: Write the Formatted String to a File <module_name>_instantiated.v (or .sv) ====
+    outputFileName = f"{module_name}_instantiated" + moduleFileType # name of the file to which we will write
+    with open(outputFileName, "w") as p: # create (or open) the file in "write" mode
+        p.write(instantiatedModule)
+        print(goodbyeMsg.format(outputFileName))
 
+    
